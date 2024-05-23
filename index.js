46d0b9600ba8f92dbad0a0c2748d38205bd69735
defyalle <164061067+defyalle@users.noreply.github.com>
2024-05-23 22:36:15 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const titleList = ["@defyalle", "#alle", "$4773", "A-L-L-E",];
+const titleList = ["dǝɟʎɐןןǝ", "𝐝𝐞𝐟𝐲𝐚𝐥𝐥𝐞", "ᴅᴇғʏᴀʟʟᴇ", "𝘥𝘦𝘧𝘺𝘢𝘭𝘭𝘦", "ｄｅｆｙａｌｌｅ",];
 let titleIndex = 0;
 
 setInterval(() => {
     titleIndex = (titleIndex + 1) % titleList.length;
     document.title = titleList[titleIndex];
-}, 1000);
+}, 100);
 
 let vid = document.getElementById("banakalsa.mp3");
 vid.volume = 0.2;
